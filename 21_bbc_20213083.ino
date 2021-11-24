e54e5cc345ddaab72c1dffa2f856034a3f4c39f4
Chanha <jch618@naver.com>
2021-11-24 10:32:51 +0900

    a

---
+++ 21_bbc_20213083.ino
@@ -0,0 +1,119 @@
+#include <Servo.h>
+
+#define PIN_IR A0
+
+// Arduino pin assignment
+#define PIN_SERVO 10
+
+// configurable parameters
+#define _DUTY_MIN 1210 // servo full clockwise position (0 degree)
+#define _DUTY_NEU 1481 // servo neutral position (90 degree)
+#define _DUTY_MAX 1752 // servo full counterclockwise position (180 degree)
+#define _DIST_ALPHA 0.5
+
+#define _POS_START (_DUTY_MIN + 100)
+#define _POS_END (_DUTY_MAX - 100)
+
+#define _SERVO_SPEED 5 // servo speed limit (unit: degree/second)
+#define INTERVAL 20  // servo update interval
+
+// global variables
+unsigned long last_sampling_time; // unit: ms
+int duty_chg_per_interval; // maximum duty difference per interval
+int toggle_interval, toggle_interval_cnt;
+float pause_time; // unit: sec
+Servo myservo;
+int duty_target, duty_curr; 
+float alpha, dist_prev, dist_ema;
+
+void setup() {
+// initialize GPIO pins
+  myservo.attach(PIN_SERVO); 
+  duty_target = duty_curr = _POS_START;
+  myservo.writeMicroseconds(duty_curr);
+  duty_curr = duty_target = 0;
+// initialize serial port
+  Serial.begin(57600);
+
+// convert angle speed into duty change per interval.
+  duty_chg_per_interval = (float)(_DUTY_MAX - _DUTY_MIN) * _SERVO_SPEED / 180 * INTERVAL / 1000;
+  //duty_chg_per..을 float로 선언하고 보았을 때 속도가 4이면 값이 0.82가 된다.
+//초마다 변하는 서보의 각도  
+
+// initialize variables for servo update.
+  pause_time = 1;
+  toggle_interval = (180.0 / _SERVO_SPEED + pause_time) * 1000 / INTERVAL;
+  toggle_interval_cnt = toggle_interval;
+  
+// initialize last sampling time
+  last_sampling_time = 0;
+
+  dist_prev = 0.0;
+}
+
+void loop() {
+  float raw_dist = ir_distance();
+  float dist_cali = 150 + 200.0 / 238 * (raw_dist - 150);
+  
+  if(dist_cali < 80 || dist_cali > 450) dist_cali = 0.0; // return 0 when out of range.
+  if(dist_cali == 0.0) dist_cali = dist_prev;
+  else dist_prev = dist_cali;
+  
+  dist_ema = alpha * dist_cali + (1 - alpha) * dist_ema;
+  
+// wait until next sampling time. 
+// millis() returns the number of milliseconds since the program started. Will overflow after 50 days.
+//  Serial.print(millis());
+//  Serial.print(last_sampling_time + INTERVAL);
+  if(millis() < last_sampling_time + INTERVAL) {
+//    Serial.print("return");
+    return;
+  }
+
+// adjust duty_curr toward duty_target by duty_chg_per_interval
+  if(duty_target > duty_curr) {
+    duty_curr += duty_chg_per_interval;
+    if(duty_curr > duty_target) duty_curr = duty_target;
+    Serial.print("yes");
+  }
+  else {
+    duty_curr -= duty_chg_per_interval;
+    if(duty_curr < duty_target) duty_curr = duty_target;
+    Serial.print("no");
+  }
+
+// update servo position
+  myservo.writeMicroseconds(duty_curr);
+
+// output the read value to the serial port
+  Serial.print("Min:1210,duty_target:");
+  Serial.print(duty_target);
+  Serial.print(",duty_curr:");
+  Serial.print(duty_curr);
+  Serial.println(",Max:1752");
+  Serial.print(dist_cali);
+//  Serial.println(duty_chg_per_interval);
+
+// toggle duty_target between _DUTY_MIN and _DUTY_MAX.
+  if(toggle_interval_cnt >= toggle_interval) {
+    toggle_interval_cnt = 0;
+    if (dist_ema < 200.0) duty_target = 1210;
+    else if (dist_ema < 250.0) duty_target = 1490;
+    else duty_target = 1752;
+  }
+  else {
+    toggle_interval_cnt++;
+  }
+//  delay(20);
+  
+
+// update last sampling time
+  last_sampling_time += INTERVAL;
+}
+
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
